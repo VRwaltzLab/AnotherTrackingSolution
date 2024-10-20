ab32703ff48490c41e5f204d930322b4ab2a279c
Alex/sprocket <Alexhb61@users.noreply.github.com>
2024-10-20 14:51:08 -0700

    Update Prototype1.hs
    Wrote to use different approach to color, but still need to include multiple gamma

--- Prototype1.hs
+++ Prototype1.hs
@@ -1,46 +1,53 @@
-data Pixel =  RGB Byte Byte Byte
-data Color = ???
-data NamedGraph = Name Color GridGraph
+import Graphite
+data Color =  RGB Byte Byte Byte
+data Pixel = Point Color Int Int
 data CameraPoint = Point Color Real Real
 data IRLPoint = Point Color Real Real Real
-GetImage :: IO Matrix Pixel
-GetViews :: IO [Matrix Pixel]
-SplitImage:: (Pixel -> Color) -> Matrix Pixel ->  [NamedGraph]
-RoughC:: Pixel -> Color
-GammaC:: [Int]-> Pixel -> Color
-GetGamma :: IO [Int]
-ContiguousParts:: [NamedGraph] -> [NamedGraph]
-IsRoughBall:: NamedGraph -> Bool
-IsSmoothBall:: NamedGraph -> Bool
-GrabCenter:: NamedGraph -> CameraPoint
-UPnP:: [CameraPoint] -> [IRLPoint] -> (Matrix Real, Matrix Real)
-mPnP:: [(Matrix Real, Matrix Real)] -> [[CameraPoint]] -> [IRLPoint] ->  Matrix Real
-RayIntersect:: [CameraPoint] -> [(Matrix Real, Matrix Real)] -> IRLPoint
-StoreCamera:: (Matrix Real,Matrix Real) -> IO ()
-StoreTracker::[IRLPoint] -> IO ()
-StorePositions:: [Matrix Real] -> IO()
-GetCalibrator:: IO [IRLPoint]
-GetTrackers:: IO [[IRLPoint]]
-ColorSort:: [[CameraPoint]] -> [[CameraPoint]]
-ColorArrange::[[CameraPoint]] ->[[[CameraPoint]]]
-GetPositions:: IO [(Matrix Real, Matrix Real)]
-FindCamera:: IO ()
-FindCamera = do singleFrame <- GetImage
-                calibrationFormat <- GetCalibrator
-                StoreCamera (UPnP (map GrabCenter (filter IsSmoothBall (ContiguousParts (SplitImage RoughC singleFrame) ) ) ) calibrationFormat )
-MeasureGamma::IO ()
-MeasureTracker:: IO ()
-MainProcess::[Matrix Pixel]-> [Int] ->[[CameraPoint]]
-MainProcess x y = map (map GrabCenter) . (filter IsRoughBall). ContiguousParts .(SplitImage (GammaC y) ) x
-MeasureTracker = do manyFrames <- GetViews
-                    curve <- GetGamma
+type Gamma = ([Int],[Int],[Int])
+type NamedGraph = UGraph Pixel Int
+type CameraChar = (Matrix Real, Matrix Real)
+type ColorMatcher = Color -> Color -> Bool
+type ColorStat = [Color] -> Color
+getImage :: IO Matrix Color
+getViews :: IO [Matrix Color]
+getGammas :: IO [Gamma]
+getCalibrator:: IO [IRLPoint]
+getTrackers:: IO [[IRLPoint]]
+getCameraPositions:: IO [CameraChar]
+generateGridGraph:: ColorMatcher -> Matrix Color -> NamedGraph
+roughColorMatch:: ColorMatcher-- Be careful this doesn't work as an equality measure (non transitive)
+gammaColorMatch:: Gamma -> ColorMatcher
+roughColorMean:: ColorStat
+gammaColorMean:: Gamma -> ColorStat
+getConnectedComponents:: UGraph v e -> [UGraph v e] -- MIGHT NEED TYPE CONSTRAINTS
+isRoughBall:: NamedGraph -> Bool
+isSmoothBall:: NamedGraph -> Bool
+isMonochrome:: ColorMatcher -> ColorStat -> NamedGraph -> Bool
+grabCenter:: NamedGraph -> CameraPoint
+uPnP:: ColorMatcher -> [CameraPoint] -> [IRLPoint] -> CameraChar --NEEDS TO DO COLOR SORTING AT START
+mPnP:: ColorMatcher -> [CameraChar] -> [[CameraPoint]] -> [IRLPoint] ->  Matrix Real --NEEDS TO DO COLOR SORTING AT START
+getRayIntersect:: ColorMatcher -> [CameraPoint] -> [CameraChar] -> IRLPoint --NEEDS TO DO COLOR SORTING AT START
+storeCamera:: CameraChar -> IO ()
+storeTracker::[IRLPoint] -> IO ()
+storePositions:: [Matrix Real] -> IO()
+arrangeByTracker:: ColorMatcher -> [[CameraPoint]] -> [[[CameraPoint]]]
+findCamera:: IO ()
+findCamera = do singleFrame <- getImage
+                calibrationPoints <- getCalibrator
+                storeCamera (uPnP roughColorMatch (map grabCenter (filter (isMonochrome roughColorMatch roughColorMean) (filter isSmoothBall (getConnectedComponents (generateGridGraph roughColorMatch singleFrame) ) ) ) ) calibrationPoints )
+measureGamma::IO ()
+measureTracker:: IO ()
+mainProcess::[Matrix Colorl]-> Gamma ->[[CameraPoint]]
+mainProcess x y = map (map GrabCenter) . (filter (isMonochrome (gammaColorMatch y ) (gammaColorMean y) ) ) . (filter isRoughBall). getConnectedComponents .(generateGridGraph (gammaColorMatch y) ) x
+measureTracker = do manyFrames <- getViews
+                    curves <- getGammas
                     cameraPositions <- GetPositions
-                    StoreTracker (map (\x -> RayIntersect x cameraPositions) (ColorSort (MainProcess manyViews curve) ) )
-TrackAllTrackers::IO ()
-TrackAllTrackers = do 
+                    storeTracker (map (\x -> RayIntersect x cameraPositions)  (mainProcess manyViews curves)  )
+trackAllTrackers::IO ()
+trackAllTrackers = do 
                     manyFrames <- GetViews
                     curve <-GetGamma
                     cameraPositions <-GetPositions
                     trackerArrangements <- GetTrackers
-                    let ptbyTrackerbyCamerabyColor = ColorArrange (MainProcess manyViews curve) in 
-                      map  (\(x,y) -> mPnP cameraPositions x y) (zip trackerArrangements ptbyTrackerbyCamerabyColor)
+                    let ptbyTrackerbyCamerabyColor = arrangeByTracker (gammaColormatch curve) (MainProcess manyViews curve) in 
+                      map  (\(x,y) -> mPnP (gammaColormatch curve) cameraPositions x y) (zip trackerArrangements ptbyTrackerbyCamerabyColor)
