440a91742afcc02248ef8eefadcfe9697d33a2b2
Alex/sprocket <Alexhb61@users.noreply.github.com>
2024-10-22 10:01:20 -0700

    Update Prototype1.hs
    Color became Shade
    Hue became Color
    Made toColor function

--- Prototype1.hs
+++ Prototype1.hs
@@ -2,21 +2,28 @@ import Graphite
 import Data.List
 roughEquals:: Real -> Real -> Bool
 roughEquals a b =  (a*1.05 > b &&  b >= a ) || (b*1.05 > a  && a >= b ) --1.05 is a 5 percent margin or error
-data Color = RGB Real Real Real --SOMETIMES USES PHOTON AND SOMETIMES USES Computer Numbers
-instance Eq Color where
-  (RGB r g b) == (RGB a d c) = (roughEquals r a) && (roughEquals g d) && (roughEquals b c)
-instance AdditiveGroup Color where
+data Shade = RGB Real Real Real --SOMETIMES USES PHOTON AND SOMETIMES USES Computer Numbers
+--always between 0.0 and 1.0 inclusive
+instance Eq Shade where
+  (RGB r g b) == (RGB r' g' b') = (roughEquals r r') && (roughEquals g g') && (roughEquals b b')
+instance AdditiveGroup Shade where
   zeroV = (RGB 0.0 0.0 0.0 )
   (RGB r g b) + (RGB r' g' b') = (RGB (r+r') (g+g') (b+b') )
-instance VectorSpace Color where
-  Scalar Color = Real
+instance VectorSpace Shade where
+  Scalar Shade = Real
   (*^) s (RGB r g b) = (RGB s*r s*g s*b)
-data Hue =  RB Real Real
-instance Eq Hue where
-  (RB r b) == (RB r' b') = (roughEquals r r') && (roughEquals b b')
-(RB r b ) == (RGB r' g' b') = (RB r b) == (RB (r'/g') (b'/g') )
-(RGB r g b) == (RB r' b') = (RB (r/g) (b/g) ) == (RB r' b')
-data CameraPoint = Point Color Real Real
+  
+data Color =  RBoverG Real Real 
+-- | RGoverB Real Real | GBoverR Real Real | Black
+instance Eq Color where
+  (RBoverG r b) == (RBoverG r' b') = (roughEquals r r') && (roughEquals b b')
+ -- (RGoverB r g) == (RBoverG r' g') = (roughEquals r r') && (roughEquals g g')
+ -- (GBoverR g b) == (GBoverR g' b') = (roughEquals g g') && (roughEquals b b') 
+ 
+toColor:: Shade -> Color
+toColor (RGB r g b) = (RBoverG (r/g) (b/g) )
+
+data CameraPoint = Point Shade Real Real
 instance AdditiveGroup CameraPoint where
   zeroV = (Point (RGB 0.0 0.0 0.0) 0.0 0.0 )
   (Point c u v ) + (Point c' u' v') = (Point (c + c') (u + u') (v + v') )
@@ -32,19 +39,20 @@ instance Ord CameraPoint where --needed for weird graph algorithms
     ((r==r') && (g==g') && (b<b') ) ||
     ((r==r') && (g==g') && (b==b') && (u<u') ) ||
     ((r==r') && (g==g') && (b==b') && (u==u') && (v<=v')
-instance Hashable CameraPoint where 
-  ????
-data WierdPoint = Point Color Real Real Real
-data IRLPoint = Point Hue Real Real Real
+--Hopefully hashable generic
+data WierdPoint = Point Shade Real Real Real
+data IRLPoint = Point Color Real Real Real
 type Gamma = ([Int],[Int],[Int])
 type CameraChar = (Matrix Real, Matrix Real)
-getImage :: IO Matrix Color --USE IDENTITY FUNCTION TO GO FROM RGB to Color
-getViews :: [Gamma] -> IO [Matrix Color] --DO GAMMA CORRECTION HERE
+
+getImage :: IO Matrix Shade --USE IDENTITY FUNCTION TO GO FROM RGB to Shade
+getViews :: [Gamma] -> IO [Matrix Shade] --DO GAMMA CORRECTION HERE
 getGammas :: IO [Gamma]
 getCalibrator:: IO [WeirdPoint]
 getTrackers:: IO [[IRLPoint]]
 getCameraPositions:: IO [CameraChar]
-generateGridGraph:: Matrix Color -> UGraph CameraPoint Int
+
+generateGridGraph:: Matrix Shade -> UGraph CameraPoint Int
 
 getConnectedComponents::(Hashable v, Eq v, Ord v) => UGraph v e -> [UGraph v e] -- MIGHT NEED TYPE CONSTRAINTS
 getConnectedComponents g = 
@@ -66,16 +74,16 @@ isSmoothBall:: UGraph CameraPoint Int -> Bool
 
 mean:: (VectorSpace v, s ~ Scalar v, Fractional s) => [v] -> v
 mean xs = (sum xs) / (fromInteger length xs) 
-isMonochrome:: UGraph CameraPoint Int -> Bool -- Converts to Hue
+isMonochrome:: UGraph CameraPoint Int -> Bool -- Converts to Color
 isMonochrome g = 
-  let colors = map (\(Point c u v) -> c) (verticies g) in
-    let (RGB r g b) =  mean colors in
-      all (== (RB (r/g) (b/g) ) ) colors
+  let shades = map (\(Point s u v) -> s) (verticies g) in
+    let c = toColor mean shades in
+      all (== c) . map toColor shades
 isMonoShaded:: UGraph CameraPoint Int -> Bool -- Doesn't convert to hue
 isMonoShaded g = 
-  let colors = map (\(Point c u v) -> c) (verticies g) in
-    let meanColor = mean colors in 
-      all (== meanColor) colors
+  let shades = map (\(Point s u v) -> s) (verticies g) in
+    let meanShade = mean shades in 
+      all (== meanShade) shades
 uPnP:: [CameraPoint] -> [WierdPoint] -> CameraChar --NEEDS TO DO COLOR SORTING AT START
 
 mPnP:: [CameraChar] -> [[CameraPoint]] -> [IRLPoint] ->  Matrix Real --NEEDS TO DO COLOR SORTING AT START
@@ -91,9 +99,10 @@ findCamera = do singleFrame <- getImage
                 calibrationPoints <- getCalibrator
                 storeCamera (uPnP (map mean(filter isMonoShaded (filter isSmoothBall (getConnectedComponents (generateGridGraph singleFrame) ) ) ) ) calibrationPoints )
 measureGamma::IO () --Strategy undefined
-measureTracker:: IO ()
-mainProcess:: Matrix Color ->[CameraPoint]
+
+mainProcess:: Matrix Shade ->[CameraPoint]
 mainProcess = (map mean) . (filter isMonochrome) . (filter isRoughBall) . getConnectedComponents . generateGridGraph
+measureTracker:: IO ()
 measureTracker = do curves <- getGammas
                     manyFrames <- getViews curves
                     cameraPositions <- getPositions
